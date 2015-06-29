b838baae74690a39ff04deeba732f523f4de1f92
Teresa Lenz <teresa.lenz@desy.de>
2015-06-29 18:54:23 +0200

    patJetJelper Class added

--- Ntuples/MyNtuple/src/.undo.classes.h
+++ Ntuples/MyNtuple/src/.undo.classes.h
@@ -1,7 +1,10 @@
 //
 //--------------------------------------------------------------------
 #include "Ntuples/MyNtuple/interface/recoTrackHelper.h"
+#include "Ntuples/MyNtuple/interface/patElectronHelper.h"
+
 namespace
 {
   HelperFor<reco::Track> t_recoTrackHelper;
+  HelperFor<pat::Electron> t_patElectronHelper;
 }
