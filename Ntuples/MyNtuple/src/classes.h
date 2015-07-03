d3ca4dd13476f8ecdb3d2252163afed96fb9af25
Teresa Lenz <teresa.lenz@desy.de>
2015-07-03 11:30:16 +0200

    add edmEventHelperExtra class

--- Ntuples/MyNtuple/src/classes.h
+++ Ntuples/MyNtuple/src/classes.h
@@ -3,6 +3,7 @@
 #include "Ntuples/MyNtuple/interface/recoTrackHelper.h"
 #include "Ntuples/MyNtuple/interface/patElectronHelper.h"
 #include "Ntuples/MyNtuple/interface/patJetHelper.h"
+#include "Ntuples/MyNtuple/interface/edmEventHelperExtra.h"
 
 
 namespace
@@ -10,4 +11,5 @@ namespace
   HelperFor<reco::Track> t_recoTrackHelper;
   HelperFor<pat::Electron> t_patElectronHelper;
   HelperFor<pat::Jet> t_patJetHelper;
+  HelperFor<edm::Event> t_edmEventHelperExtra;
 }
