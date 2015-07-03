4ed691ec87850815ef85a5469cb901c0a084c278
Teresa Lenz <teresa.lenz@desy.de>
2015-07-03 12:44:26 +0200

    removed unnecessary line

--- makeNtupleFromALCARECO_cfg.py
+++ makeNtupleFromALCARECO_cfg.py
@@ -196,7 +196,6 @@ process.schedule = cms.Schedule(process.pPF)
 
 
 ## Dump python config if wished
-outfile = open('dumpALCARECO.py','w'); print >> outfile,process.dumpPython(); outfile.close()
 if(runOnMC):
   outfile = open('dumpALCARECO_MC.py','w'); print >> outfile,process.dumpPython(); outfile.close()
 else:
