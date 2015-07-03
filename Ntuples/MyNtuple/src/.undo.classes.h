9f122c751a75859d5ccea161f189fc702f299e79
Teresa Lenz <teresa.lenz@desy.de>
2015-07-03 11:30:55 +0200

    add patJetHelper.h

--- Ntuples/MyNtuple/src/.undo.classes.h
+++ Ntuples/MyNtuple/src/.undo.classes.h
@@ -2,9 +2,12 @@
 //--------------------------------------------------------------------
 #include "Ntuples/MyNtuple/interface/recoTrackHelper.h"
 #include "Ntuples/MyNtuple/interface/patElectronHelper.h"
+#include "Ntuples/MyNtuple/interface/patJetHelper.h"
+
 
 namespace
 {
   HelperFor<reco::Track> t_recoTrackHelper;
   HelperFor<pat::Electron> t_patElectronHelper;
+  HelperFor<pat::Jet> t_patJetHelper;
 }
