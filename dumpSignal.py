8a21a4fbd91fa73b38976b57229137a8ba47c498
Teresa Lenz <teresa.lenz@desy.de>
2015-07-03 15:10:17 +0200

    change input file

--- dumpSignal.py
+++ dumpSignal.py
@@ -6,7 +6,7 @@ process.source = cms.Source("PoolSource",
     noEventSort = cms.untracked.bool(True),
     skipBadFiles = cms.untracked.bool(True),
     duplicateCheckMode = cms.untracked.string('noDuplicateCheck'),
-    fileNames = cms.untracked.vstring('file:TTJets_skimmed.root'),
+    fileNames = cms.untracked.vstring('file:RECO_RAW2DIGI_L1Reco_RECO_PU_MG_mass_100_ctau_1cm_0.root'),
     inputCommands = cms.untracked.vstring('keep *', 
         'drop GenLumiInfoProduct_*_*_*')
 )
